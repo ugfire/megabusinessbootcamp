b3e3252a859961f8fb988c3b7f861e02b1c29e36
alfredochola <ochalfie@gmail.com>
2023-06-08 10:00:32 +0300

    minor edits

--- js/swal.js
+++ js/swal.js
@@ -1,24 +1,3 @@
 function sendMessage() {
-  document.getElementById("sendButton").addEventListener("click", function () {
-    // Show "sending" popup
-    Swal.fire({
-      title: "Sending...",
-      icon: "info",
-      showConfirmButton: false,
-      allowOutsideClick: false,
-    });
-
-    // Simulate a delay of 2 seconds
-    setTimeout(function () {
-      // Close the "sending" popup
-      Swal.close();
-
-      // Show "success" popup
-      Swal.fire({
-        title: "Success!",
-        text: "Message sent successfully.",
-        icon: "success",
-      });
-    }, 2000);
-  });
+  Swal.fire("Success", "Your message has been sent", "success");
 }
